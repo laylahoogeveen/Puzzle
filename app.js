c09859a30df209b98499e028879c90c9a4102203
laylahoogeveen <layla-hoogeveen@hotmail.com>
2022-06-27 11:23:25 +0200

    hosting #11

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 // app.use('/css', express.static(__dirname + '/Images'));
 app.use(express.static('public'));
 const json_puzzle = require('./json/modified/1.json');
+app.listen(process.env.PORT || 3000);
+
 app.engine('pug', require('pug').__express);
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/public/views');
